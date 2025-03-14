ad764b5ccd0cb72d09ff0ff0aaf026ec2c559607
Andrea Buonaurio <65712954+andreabuon@users.noreply.github.com>
2025-03-14 10:08:56 +0100

    feat: implement findMostFrequentedArea

--- RouteAnalyzer/src/main/kotlin/utils/req_MostFrequentedArea.kt
+++ RouteAnalyzer/src/main/kotlin/utils/req_MostFrequentedArea.kt
@@ -3,7 +3,31 @@ package org.example.util
 import org.example.model.Waypoint
 
 fun findMostFrequentedArea(waypointList: List<Waypoint>, areaRadiusKm: Double): Pair<Waypoint, Int> {
-    // TODO: calculate all distances first in a matrix and then for each waypoint
-    //  calculate how many are closer than areaRadiusKm; at last find the most frequented one
-    TODO()
+    if (waypointList.isEmpty()) {
+        throw Exception("Waypoint list is empty.")
+    }
+
+    val earthRadius = 6371.0 //FIXME
+
+    var center_waypoint = waypointList[0]
+    var center_waypoint_neighbours_count = 0
+    for (waypoint1 in waypointList) {
+        var neighbours_count = 0
+
+        for (waypoint2 in waypointList) {
+            if (waypoint2.equals(waypoint1))
+                continue
+
+            if (haversine( waypoint1.latitude, waypoint1.longitude, waypoint2.latitude, waypoint2.longitude, earthRadius) < areaRadiusKm) {
+                neighbours_count += 1
+            }
+
+        }
+        if(neighbours_count > center_waypoint_neighbours_count) {
+            center_waypoint = waypoint1
+            center_waypoint_neighbours_count = neighbours_count
+        }
+    }
+
+    return Pair(center_waypoint, center_waypoint_neighbours_count)
 }
