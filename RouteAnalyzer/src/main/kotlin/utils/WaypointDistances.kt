1c5baa71ef6a8c7992df2864ba8599223749b2a2
Andrea Buonaurio <65712954+andreabuon@users.noreply.github.com>
2025-03-26 16:44:56 +0100

    style: fixed typo

--- RouteAnalyzer/src/main/kotlin/utils/WaypointDistances.kt
+++ RouteAnalyzer/src/main/kotlin/utils/WaypointDistances.kt
@@ -6,15 +6,15 @@ import kotlin.math.*
 
 /**
  * Returns the distance between two waypoints using the Haversine formula.
- * @param waypint1 the first waypoint
+ * @param waypoint1 the first waypoint
  * @param waypoint2 the second waypoint
  * @return the distance between the two waypoints
  */
-fun haversine(waypint1: Waypoint, waypoint2: Waypoint): Double {
-    val dLat = Math.toRadians(waypoint2.latitude - waypint1.latitude)
-    val dLon = Math.toRadians(waypoint2.longitude - waypint1.longitude)
+fun haversine(waypoint1: Waypoint, waypoint2: Waypoint): Double {
+    val dLat = Math.toRadians(waypoint2.latitude - waypoint1.latitude)
+    val dLon = Math.toRadians(waypoint2.longitude - waypoint1.longitude)
     val a =
-        sin(dLat / 2).pow(2.0) + cos(Math.toRadians(waypint1.latitude)) * cos(Math.toRadians(waypoint2.latitude)) * sin(
+        sin(dLat / 2).pow(2.0) + cos(Math.toRadians(waypoint1.latitude)) * cos(Math.toRadians(waypoint2.latitude)) * sin(
             dLon / 2
         ).pow(2.0)
     val c = 2 * asin(sqrt(a))
